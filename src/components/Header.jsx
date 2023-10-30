dd5d28b407b015e7c2a01f2e52bfc3494b9dfc92
sivaShilpa <shilpa.vchand@gmail.com>
2023-10-30 15:38:07 -0400

    feat: header complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,53 @@
 "use client"
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import '../styles/Header.css'
 import Image from 'next/image'
 import homeLogo from "@/assets/homeLogo.svg"
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 import HeaderMiddleLink from './HeaderMiddleLink'
+import diamondBlue from '../assets/diamondBlue.svg'
+import coinGold from '../assets/coinGold.svg'
+import avatar from '../assets/avatar.svg'
+import status from '../assets/status.svg'
 
 const Header = () => {
-        
-  return (
-    <>
-        <div className='header'>
-            <Link className='headerLeft' href="/">
-                <Image src={homeLogo} alt="logoHome" />
-            </Link>              
-            <div className='headerMiddle'>
-                <HeaderMiddleLink icon={'🚀'} text={'Missions'} path={'/missions'}/>
-                <HeaderMiddleLink icon={'🏆'} text={'Leaderboard'} path={'/leaderboard'}/>
-                <HeaderMiddleLink icon={'💰'} text={'Rewards'} path={'/rewards'}/>
+    const [flag, setFlag] = useState(false);    
+
+    return (
+        <>
+            <div className='header'>
+                <Link className='headerLeft' href="/">
+                    <Image src={homeLogo} alt="logoHome" />
+                </Link>     
+                {/* <div className="frame">          */}
+                    <div className='headerMiddle'>
+                        <HeaderMiddleLink icon='🚀' text='Missions' path='/missions' onClick={() => setFlag(currentState => !currentState)} flag={flag}/>
+                        <HeaderMiddleLink icon='🏆' text='Leaderboard' path='/leaderboard' onClick={() => setFlag(currentState => !currentState)} flag={flag}/>
+                        <HeaderMiddleLink icon='💰' text='Rewards' path='/rewards' onClick={() => setFlag(currentState => !currentState)} flag={flag}/>
+                    </div>
+                    
+                    <div className="headerRight">
+                        <div className="diamonds">
+                            <div className='icon'>
+                                <Image src={diamondBlue} alt="diamond"/>
+                            </div>
+                            <div className='num'>20,000</div>    
+                        </div>
+                        <div className="coins">
+                            <div className='icon'>
+                                <Image src={coinGold} alt="coin"/>
+                            </div>
+                            <div className='num'>11,000</div>    
+                        </div>
+                        <div className='Avatar'>                       
+                            <Image src={avatar} alt="avatar" className="avatar"/>
+                            <Image src={status} alt="status" className='status' />                        
+                        </div>
+                    </div>
+                {/* </div>             */}
             </div>
-            <div className="headerRight">
-                <div>diamonds</div>
-                <div>coins</div>
-                <div>avatar</div>
-            </div>            
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Header
